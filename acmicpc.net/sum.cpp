e115fecc96e1ecfd277b8ec84437d8e700d14480
SY <sy.ahn25@gmail.com>
2016-10-19 18:08:29 +0900

    fix: sum

--- acmicpc.net/sum.cpp
+++ acmicpc.net/sum.cpp
@@ -1,19 +1,26 @@
 // # Reflection
 
-// too trivial, but It should be better algorithm for summation.
+// too trivial, but I couldn't come up with summation notation. And It gives huge win without looping. 
 
 // # Problem
-
+//
 // 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
 // 1 초	128 MB	7833	5628	5073	73.352%
+//
 // 문제
-// n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+// John got a bad mark in math. The teacher gave him another task. John is to write a program which computes the sum of integers from 1 to n. If he manages to present a correct program, the bad mark will be cancelled.
+//
+// Write a program which:
+//
+// reads the number n from the standard input,
+// computes the sum of integers from 1 to n,
+// writes the answer to the standard output.
 //
 // 입력
-// 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+// The first and only line of the standard input contains one integer n (1 ≤ n ≤ 10 000).
 //
 // 출력
-// 1부터 n까지 합을 출력한다.
+// One integer is to be written to the standard output. This integer should be the sum of integers from 1 to n.
 
 // # Answer
 
@@ -35,3 +42,13 @@ int main() {
 }
 
 // # Improved version
+
+#include <iostream>
+using namespace std;
+
+int main() {
+		int n;
+		cin >> n;
+    cout << n * (n+1)/2;
+    return 0;
+}
