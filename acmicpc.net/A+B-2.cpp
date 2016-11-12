853ead67372b030228bf665ba9f6cdc7276085a6
SY <sy.ahn25@gmail.com>
2016-11-13 08:37:04 +0900

    solved: A+B-2

---
+++ acmicpc.net/A+B-2.cpp
@@ -0,0 +1,33 @@
+// # Reflection
+
+// Too trivial
+
+// # Problem
+
+// 시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+// 1 초	128 MB	5095	3809	3500	77.076%
+// 문제
+// A+B를 계산하시오.
+//
+// 입력
+// 첫째 줄에 A, 둘째 줄에 B가 주어진다. (0 < A, B < 10)
+//
+// 출력
+// 첫째 줄에 A+B를 출력한다. (A+B < 10)
+
+// # Answer
+
+// # Initial version
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a, &b);
+    cout << a + b;
+    return 0;
+}
+
+// # Improved version
