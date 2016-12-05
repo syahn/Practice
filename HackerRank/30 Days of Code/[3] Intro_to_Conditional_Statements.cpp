dad31f42ef8dc5b8401e4658d0c360eb167f6687
SY <sy.ahn25@gmail.com>
2016-12-05 20:18:11 +0900

    fixed: Intro to Conditional Statements

--- HackerRank/30 Days of Code/[3] Intro_to_Conditional_Statements.cpp
+++ HackerRank/30 Days of Code/[3] Intro_to_Conditional_Statements.cpp
@@ -1,6 +1,8 @@
 // # Reflection
 
 // - Too trivial, but I couldn't pass it one time. What a shame.
+// - And actually, there was better way to solve this. Even though it seems trivial,
+//   do your best to optimize and come up with better solution.
 
 // # Problem
 
@@ -41,12 +43,6 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-// If  is odd, print Weird
-// If  is even and in the inclusive range of  to , print Not Weird
-// If  is even and in the inclusive range of  to , print Weird
-// If  is even and greater than , print Not Weird
-// Complete the stub code provided in your editor to print whether or not  is weird.
-
 
 int main(){
     int N;
@@ -59,6 +55,36 @@ int main(){
     return 0;
 }
 
-
-
 // # Improved solution
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    if( N%2 || N >= 6 && N <= 20) cout << "Weird";
+    else cout <<"Not Weird";
+
+    return 0;
+}
