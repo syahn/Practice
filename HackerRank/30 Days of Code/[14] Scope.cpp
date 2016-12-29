2bd7b3a24e9b386459fdc7024b220ba800bb525a
SY <sy.ahn25@gmail.com>
2016-12-29 19:50:43 +0900

    solved: scope

--- HackerRank/30 Days of Code/[14] Scope.cpp
+++ HackerRank/30 Days of Code/[14] Scope.cpp
@@ -1,13 +1,131 @@
 // # Reflection
 
-
+// - There is convenient method for calculating max and min in the vector
 
 // # Problem
+
+// Objective
+// Today we're discussing scope. Check out the Tutorial tab for learning materials
+// and an instructional video!
+//
+// The absolute difference between two integers,  and , is written as .
+// The maximum absolute difference between two integers in a set of positive integers,
+// , is the largest absolute difference between any two integers in .
+//
+// The Difference class is started for you in the editor. It has a private integer
+// array () for storing  non-negative integers, and a public integer () for storing the maximum absolute difference.
+//
+// Task
+// Complete the Difference class by writing the following:
+//
+// A class constructor that takes an array of integers as a parameter and saves it to the  instance variable.
+// A computeDifference method that finds the maximum absolute difference between
+// any  numbers in  and stores it in the  instance variable.
 
 
 
 // # Initial solution
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+
+  	public:
+  	int maximumDifference;
+
+    Difference(vector<int> &arr){
+        elements = arr;
+    }
+
+    void computeDifference(){
+        int len = elements.size();
+        int temp, max = 0;
+        for(int i=0; i<len;  i++){
+            for(int j=0; j<len; j++){
+                temp = abs(elements[i] - elements[j]);
+                if(maximumDifference < temp)
+                    maximumDifference = temp;
+            }
+        }
+    }
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> a;
+
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+
+        a.push_back(e);
+    }
+
+    Difference d(a);
+
+    d.computeDifference();
+
+    cout << d.maximumDifference;
+
+    return 0;
+}
 
 // # Improved solution
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+
+  	public:
+  	int maximumDifference;
+
+    Difference(vector<int> &arr){
+        elements = arr;
+    }
+
+    void computeDifference(){
+        int maxNum = *max_element(elements.begin(), elements.end());
+        int minNum = *min_element(elements.begin(), elements.end());
+        maximumDifference = maxNum-minNum;
+    }
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> a;
+
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+
+        a.push_back(e);
+    }
+
+    Difference d(a);
+
+    d.computeDifference();
+
+    cout << d.maximumDifference;
+
+    return 0;
+}
--- HackerRank/30 Days of Code/[15] Linked_list.cpp
+++ HackerRank/30 Days of Code/[15] Linked_list.cpp
@@ -4,10 +4,54 @@
 
 // # Problem
 
+// A Node class is provided for you in the editor. A Node object has an integer data field,
+// , and a Node instance pointer, , pointing to another node (i.e.: the next node in a list).
+//
+// A Node insert function is also declared in your editor. It has two parameters: a pointer,
+// , pointing to the first node of a linked list, and an integer  value that must be
+// added to the end of the list as a new Node object.
+//
+// Task
+// Complete the insert function in your editor so that it creates a new Node
+// (pass  as the Node constructor argument) and inserts it at the tail of the linked
+// list referenced by the  parameter. Once the new node is added, return the reference to the  node.
+
+
 
 
 // # Initial solution
 
+#include <iostream>
+#include <cstddef>
+using namespace std;
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+////////////solution////////////////
+Node* insert(Node *head,int data)
+{
+    Node* newNode = new Node(data);
+    Node* tail = head;
+    if(head != NULL){
+      while(tail->next != NULL){
+          tail = tail->next;
+      }
+      tail->next = newNode;
+      return head;
+    } else {
+        return newNode;
+    }
+}
 
 
 // # Improved solution
