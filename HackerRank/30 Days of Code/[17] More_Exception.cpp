de8be68e05e27872a54fa3eee9d6905bf065b656
SY <sy.ahn25@gmail.com>
2017-01-02 10:31:04 +0900

    solved: More Exception

--- HackerRank/30 Days of Code/[17] More_Exception.cpp
+++ HackerRank/30 Days of Code/[17] More_Exception.cpp
@@ -1,13 +1,38 @@
 // # Reflection
 
-
+// - Don't forget that I need to specify exception e in reference of pointer.
 
 // # Problem
 
+// Objective
+// Yesterday's challenge taught you to manage exceptional situations by using try
+// and catch blocks. In today's challenge, you're going to practice throwing and propagating an exception.
+// Check out the Tutorial tab for learning materials and an instructional video!
+//
+// Task
+// Write a Calculator class with a single method: int power(int,int).
+// The power method takes two integers,  and , as parameters and returns the integer result of .
+// If either  or  is negative, then the method must throw an exception with the message: n and p should be non-negative.
 
 
 // # Initial solution
+#include <stdexcept>
+class Calculator{
+    public:
+    int power(int n, int p){
+        if(n<0 || p<0){
+            throw invalid_argument("n and p should be non-negative");
+        }
+        return pow(n, p);
+    }
+};
 
-
+try{
+      int ans=myCalculator.power(n,p);
+      cout<<ans<<endl;
+}
+catch(exception& e){
+    cout<<e.what()<<endl;
+}
 
 // # Improved solution
