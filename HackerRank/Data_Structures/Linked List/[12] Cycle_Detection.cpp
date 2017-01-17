c41cb9e2112c6b76a7f2b474ffdabbc11cca5969
SY <sy.ahn25@gmail.com>
2017-01-17 20:00:23 +0900

    edit: cycle_detection

--- HackerRank/Data_Structures/Linked List/[12] Cycle_Detection.cpp
+++ HackerRank/Data_Structures/Linked List/[12] Cycle_Detection.cpp
@@ -1,9 +1,11 @@
 // # Reflection
 
+
 // - At first, I thought it has bigger size of the gap than 2. But it turns out,
 //   it has just gap as 2.
 // - It could be solved with runner technic.
-
+// ####important####
+// - I was wrong about it. it detects larger loop too.
 
 // # Problem
 
@@ -20,15 +22,13 @@
 
 bool has_cycle(Node* head) {
     if(head == NULL) return false;
-        Node* runner = head;
-        Node* follower = head;
-        int count = 0;
-        int gap = 2;
-        while(runner != NULL){
-            runner = runner->next->next;
-            if(runner == follower) return true;
-            follower = follower->next;
-        }
+    Node* runner = head;
+    Node* follower = head;
+    while(runner != NULL){
+        runner = runner->next->next;
+        if(runner == follower) return true;
+        follower = follower->next;
+    }
     return false;
 }
 
