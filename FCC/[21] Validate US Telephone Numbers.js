6f5dd5a84922e2bbf2cf2ab5e71f87ed472b7256
SY <sy.ahn25@gmail.com>
2017-02-15 14:31:31 +0900

    failed: Validate US phone Number
    - I failed to solve it. I should study regex!!

---
+++ FCC/[21] Validate US Telephone Numbers.js
@@ -0,0 +1,31 @@
+// 1. Reflection
+
+// - I failed to solve it. I should study regex!!
+
+// 2. Problem
+
+// Return true if the passed string is a valid US phone number.
+//
+// The user may fill out the form field any way they choose as long as it is a valid US number.
+// The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+//
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+// For this challenge you will be presented with a string such as 800-692-7753 or
+// 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number
+// based on any combination of the formats provided above. The area code is required.
+// If the country code is provided, you must confirm that the country code is 1.
+// Return true if the string is a valid US phone number; otherwise return false.
+
+// 3. Initial solution
+
+function telephoneCheck(str) {
+   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+   return regex.test(str);
+}
+
+// 4. Improved solution
