7a2f8be44ab43cebe171a062124ee28cfa245693
Frank Ahn <frankahn9@gmail.com>
2017-04-25 14:36:12 +0900

    [Failed] Intersection LinkedList
    - Failed.
    - I solved this before, but couldn't solve it.

---
+++ LeetCode/Javascript/[160] Intersection of Two Linked Lists.js
@@ -0,0 +1,74 @@
+// 1. Reflection
+
+// - Failed. 
+// - I solved this before, but couldn't solve it. 
+
+// 2. Problem
+
+// Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+// For example, the following two linked lists:
+
+// A:          a1 → a2
+//                    ↘
+//                      c1 → c2 → c3
+//                    ↗            
+// B:     b1 → b2 → b3
+// begin to intersect at node c1.
+
+
+// Notes:
+
+// If the two linked lists have no intersection at all, return null.
+// The linked lists must retain their original structure after the function returns.
+// You may assume there are no cycles anywhere in the entire linked structure.
+// Your code should preferably run in O(n) time and use only O(1) memory.
+
+// 3. Initial solution( complexity: time=>O(N^2) space=>O(N))
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+
+// It couldn't meet time limit. 
+var getIntersectionNode = function(headA, headB) {
+    let mapA = [], mapB = [], firstMeet;
+    let i = 0, j = 0;
+    while (headA) {
+        mapA[i++] = headA;
+        headA = headA.next;
+    }
+    while (headB) {
+        let idx = mapA.indexOf(headB);
+        if (idx > -1) {
+            firstMeet = mapA[idx];
+            while(mapA[idx] === headB) {
+                idx = mapA[idx+1] ? idx + 1 : -1;
+                headB = headB.next;
+                if ( idx >= 0 && headB !== null && (mapA[idx] !== headB)) {
+                    return  null;
+                }
+            }
+            return firstMeet;
+        }
+        mapB[j++] = headB;
+        headB = headB.next;
+    }
+};
+
+// 4. Improved solution( complexity: time=>O(N) space=>O(1) )
+
+var getIntersectionNode = function(headA, headB) {
+    if (headA === null || headB === null) {
+        return null;
+    }
+    let nodeA = headA, nodeB = headB;
+    while (nodeA !== nodeB) {
+        nodeA = nodeA === null ? headB : nodeA.next;
+        nodeB = nodeB === null ? headA : nodeB.next;
+    }
+    return nodeA;
+}
