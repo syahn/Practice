c6ef3c9da5ce3fa53a3a8d4d28ec567163d60acc
Frank Ahn <frankahn9@gmail.com>
2017-05-01 09:55:15 +0900

    [solved] 237. Delete Node

---
+++ LeetCode/Javascript/[237] Delete Node in a Linked List.js
@@ -0,0 +1,32 @@
+// 1. Reflection
+
+// - Solved. Too simple. 
+// 2. Problem
+
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+
+Subscribe to see which companies asked this question.
+
+
+// 3. Initial solution( complexity: time=>O(1) space=>O(1))
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
+
+
+// 4. Improved solution( complexity: time=>O() space=>O() )
