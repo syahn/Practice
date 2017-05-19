d932b254f118083baaeebd6a348b259ed610947a
Frank Ahn <frankahn9@gmail.com>
2017-05-19 22:34:07 +0900

    [failed] List Filtering
    - I learned about isinstance

---
+++ Codewars/python/[7kyu] List Filtering.py
@@ -0,0 +1,18 @@
+// 1. Reflection
+
+# - failed
+# - I learned about isinstance
+
+// 2. Problem
+
+# In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// 3. Initial solution( complexity: time=>O() space=>O())
+
+
+
+// 4. Improved solution( complexity: time=>O() space=>O() )
+
+def filter_list(l):
+  # 'return a new list with the strings filtered out'
+  return [i for i in l if not isinstance(i, str)]
