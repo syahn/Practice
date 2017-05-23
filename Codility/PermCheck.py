e605c229b585595d6798044c317bfcfde7500a83
Frank Ahn <frankahn9@gmail.com>
2017-05-23 21:11:14 +0900

    Failed: PermCheck

---
+++ Codility/PermCheck.py
@@ -0,0 +1,70 @@
+# Reflection
+
+# - Failed. 
+# - Improved solution is very clever. It's worth to note
+# - It failed on Performance test. 
+
+# Problem
+
+# A non-empty zero-indexed array A consisting of N integers is given.
+
+# A permutation is a sequence containing each element from 1 to N once, and only once.
+
+# For example, array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# is a permutation, but array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# is not a permutation, because value 2 is missing.
+
+# The goal is to check whether array A is a permutation.
+
+# Write a function:
+
+# def solution(A)
+
+# that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+
+# For example, given array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# the function should return 1.
+
+# Given array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# the function should return 0.
+
+# Assume that:
+
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range [1..1,000,000,000].
+# Complexity:
+
+# expected worst-case time complexity is O(N);
+# expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+# Elements of input arrays can be modified.
+
+# Initial solution( complexity: time=>O() space=>O())
+
+
+# Improved solution( complexity: time=>O(N) space=>O(N) )
+
+def solution(A):
+    N = len(A)
+    return int(len(set(A)) is N and max(A) == N)
+
+
+    
+    
