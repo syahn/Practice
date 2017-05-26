a8a1117dae700039aeb4dc850388e1f044f07bc6
Frank Ahn <frankahn9@gmail.com>
2017-05-26 18:06:57 +0900

    [solved]: Dominator

--- Codility/Bracket.py
+++ Codility/Bracket.py
@@ -1,10 +1,54 @@
 # Reflection
 
 
+
+
 # Problem
+
+# A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+
+# S is empty;
+# S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+# S has the form "VW" where V and W are properly nested strings.
+# For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+
+# Write a function:
+
+# def solution(S)
+
+# that, given a string S consisting of N characters, returns 1 if S is properly nested and 0 otherwise.
+
+# For example, given S = "{[()()]}", the function should return 1 and given S = "([)()]", the function should return 0, as explained above.
+
+# Assume that:
+
+# N is an integer within the range [0..200,000];
+# string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".
+# Complexity:
+
+# expected worst-case time complexity is O(N);
+# expected worst-case space complexity is O(N) (not counting the storage required for input arguments).
+
 
 
 # Initial solution( complexity: time=>O() space=>O())
 
-
+def solution(S):
+    while '[]' in S or '{}' in S or '()' in S:
+        S = S.replace('[]', '')
+        S = S.replace('()', '')
+        S = S.replace('{}', '')
+    return S == ''
 # Improved solution( complexity: time=>O() space=>O() )
+
+def solution(S):
+  dict = { '{': '}', '(': ')', '[': ']'}
+  stk = []
+  for n in S:
+    if n in dict.keys():
+      stk.append(n)
+    else:
+      if len(stk) == 0 or dict[stk.pop()] != n: 
+      	return False
+
+  return True if len(stk) == 0 else False
