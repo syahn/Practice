f3dc3efc478414817a9e83942b5821080d9d551d
Frank Ahn <frankahn9@gmail.com>
2017-05-26 19:21:37 +0900

    [solved] MaxSlice

--- Codility/MaxSlice.py
+++ Codility/MaxSlice.py
@@ -4,7 +4,6 @@
 
 # Problem
 
-
 # Task description
 # A zero-indexed array A consisting of N integers is given. It contains daily prices of a stock share for a period of N consecutive days. If a single share was bought on day P and sold on day Q, where 0 ≤ P ≤ Q < N, then the profit of such transaction is equal to A[Q] − A[P], provided that A[Q] ≥ A[P]. Otherwise, the transaction brings loss of A[P] − A[Q].
 
@@ -42,7 +41,7 @@
 
 # expected worst-case time complexity is O(N);
 # expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
-# Elements of input arrays can be modified.
+# Elements of input arrays can be modified
 
 # Initial solution( complexity: time=>O() space=>O())
 
